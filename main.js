965135dab7ddcca735f63331f431fcad3ccaae8a
Christin Schade <christinabbe90gmail.com>
2022-08-12 11:39:25 +0200

    feat(main.js): add html elements

--- main.js
+++ main.js
@@ -9,18 +9,47 @@
 
 function isEven (number) {
   if (number % 2 == 0) {
-      return true;
+    return true;
   } else {
-      return false;
+    return false;
   }
 };
 
+const createHtmlElement = (htmlElement, cssClass) => {
+  const elementNode = document.createElement(htmlElement);
+  if (cssClass) {
+    elementNode.classList.add(cssClass);
+  }
+  return elementNode;
+}
+
 const createArticleNode = (articlesCount) => {
   const articleCssClass = isEven(articlesCount) ? 'flex-row' : 'flex-row-reverse';
+  const textContainerCssClass = isEven(articlesCount) ? 'text-mountain' : 'text-mountain-reverse';
   console.log(`We have ${articlesCount} articles`);
-  const articleNode = document.createElement("article");
-  // Das ist nur ein Test
-  articleNode.innerText = 'test';
+  const articleNode = document.createElement('article');
+  const imageContainerNode = createHtmlElement('div', 'image');
+  const imageNode = createHtmlElement('img', 'mountain-image');
+  const textContainerNode = createHtmlElement('div', textContainerCssClass);
+  const textNode = createHtmlElement('p');
+
+  imageNode.setAttribute('src', '/img/bild_1.webp');
+  imageNode.setAttribute('alt', 'mountain night');
+
+  imageContainerNode.append(imageNode);
+  textContainerNode.append(textNode);
+
+  articleNode.append(imageContainerNode);
+  articleNode.append(textContainerNode);
+
+  
+
+  // articleNode.innerText = 'test';
+ 
+/*   const myImg = document.createElement('img');
+  image.onload = function () {
+
+  } */
   console.log(articleCssClass);
   articleNode.classList.add(articleCssClass);
 
